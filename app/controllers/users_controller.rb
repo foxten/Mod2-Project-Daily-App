acf18ba3465a609a6364c9664486b415eba03c01
foxten <tenaysiafox@gmail.com>
2020-07-30 10:11:13 -0500

    Small changes to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,6 @@ class UsersController < ApplicationController
         end
     end
 
-    def broke
-        @current_user
-    end
-
     private
     def user_params
         params.require(:user).permit(:name, :password)
